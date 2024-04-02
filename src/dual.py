67a97315f350d995e99e8c995245f1d90b96e6c6
Jack-42 <johnjack0987@gmail.com>
2024-04-01 20:27:20 -0600

    minor fix to dual def

--- src/dual.py
+++ src/dual.py
@@ -9,14 +9,14 @@ from sympy import Line, Point
 
 def dual_to_line(p: Point):
     assert isinstance(p, Point), "Points only!"
-    return Line(Point(0, p.y), slope=p.x)
+    return Line(Point(0, -p.y), slope=p.x)
 
 
 def dual_to_point(l: Line):
     assert isinstance(l, Line), "Lines only!"
     y_axis = Line(Point(0, 0), Point(0, 1))
     y_intersect = l.intersection(y_axis)
-    return Point(l.slope, y_intersect[0].y)
+    return Point(l.slope, -y_intersect[0].y)
 
 
 if __name__ == "__main__":
