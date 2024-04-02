1255edbc9313841c764e0704d4cc686dcc93d50e
Jack-42 <johnjack0987@gmail.com>
2024-04-01 18:29:35 -0600

    minor cleanup

--- src/dual.py
+++ src/dual.py
@@ -4,23 +4,21 @@ Date: 3/31/2024
 Description: Entry-point for code
 """
 
-import numpy as np
-from scipy.spatial import Voronoi
-import matplotlib.pyplot as plt
-from sympy import Point, Line
+from sympy import Line, Point
 
-def dual_to_line(p : Point):
-    assert isinstance(p, Point), 'Points only!'
-    return Line (Point(0,p.y), slope=p.x)
 
-def dual_to_point(l : Line):
-    assert isinstance(l, Line), 'Lines only!'
-    y_axis = Line (Point(0, 0), Point(0, 1))
+def dual_to_line(p: Point):
+    assert isinstance(p, Point), "Points only!"
+    return Line(Point(0, p.y), slope=p.x)
+
+
+def dual_to_point(l: Line):
+    assert isinstance(l, Line), "Lines only!"
+    y_axis = Line(Point(0, 0), Point(0, 1))
     y_intersect = l.intersection(y_axis)
-    return Point (l.slope, y_intersect[0].y)
+    return Point(l.slope, y_intersect[0].y)
 
-p1 = Point (3,4)
 
-print(dual_to_point(dual_to_line(p1)))
-    
-
+if __name__ == "__main__":
+    p1 = Point(3, 4)
+    print(dual_to_point(dual_to_line(p1)))
