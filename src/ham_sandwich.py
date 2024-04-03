eff63e6e339cba655c92af98cf4dc27148b74c05
Jack-42 <johnjack0987@gmail.com>
2024-04-03 14:37:25 -0600

    add infintessimal to weights

--- src/ham_sandwich.py
+++ src/ham_sandwich.py
@@ -11,11 +11,12 @@ import numpy as np
 from sympy import Line, Point
 
 from dual import dual_to_lines
+from utils.constants import EPSILON
 
 
 def weighted_2d_hs(
     point_set,
-    weights: Optional[np.ndarray],
+    weights: Optional[np.ndarray[np.float64]] = None,
 ):
     """
     Weighted ham-sandwhich algo in 2-dimensions.
@@ -26,8 +27,19 @@ def weighted_2d_hs(
         raise NotImplementedError(
             f"Currently only support 2-color ham-sandwhich cuts, given {point_set.n_colors} colors"
         )
-    # 1) convert points to their duals (lines)
+    if weights is None:
+        weights = np.ones_like(point_set.colors, dtype=np.float64)
+    assert len(weights) > 2
+
     color_sets = point_set.color_sets
+    # add formal infintessimal to one point from each set
+    # may need to adjust depending on sensitivity (need to gurantee cut L is incident to one red, one blue pt)
+    W = weights.copy()
+    for color in point_set.unique_colors:
+        first_i = np.argmax(point_set.colors == color)
+        W[first_i] += EPSILON
+
+    # 1) convert points to their duals (lines)
     T = {
         c: dual_to_lines(c_set) for c, c_set in color_sets.items()
     }  # T[i] = set of lines for color i
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -4,8 +4,13 @@ Date: 3/31/2024
 Description: Contains constant values used throughout project
 """
 
+import numpy as np
+
 # sampling methods
 UNIFORM_RANDOM = "uni_random"
 
 # color sampling methods
 RANDOM = "random"
+
+# math
+EPSILON = np.finfo(np.float64).eps
